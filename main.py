71388cdf27183816ab9c5d18272e57e22bfd1ac1
s1030512149 <64786663+s1030512149@users.noreply.github.com>
2023-03-18 18:44:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -217,30 +217,39 @@ class YoloPredictor(BasePredictor, QObject):
     def get_annotator(self, img):
         return Annotator(img, line_width=self.args.line_thickness, example=str(self.model.names))
 
-    def preprocess(self, img):
-        img = torch.from_numpy(img).to(self.model.device)
-        img = img.half() if self.model.fp16 else img.float()  # uint8 to fp16/32
-        img /= 255  # 0 - 255 to 0.0 - 1.0
-        return img
-
     def postprocess(self, preds, img, orig_img):
         ### important
-        preds = ops.non_max_suppression(preds,
+        p = ops.non_max_suppression(preds[0],
                                         self.conf_thres,
                                         self.iou_thres,
                                         agnostic=self.args.agnostic_nms,
                                         max_det=self.args.max_det,
-                                        classes=self.args.classes)
+                                        classes=self.args.classes,
+                                        nc=len(self.model.names),)
 
         results = []
-        for i, pred in enumerate(preds):
+        # print('preds[0]:', p.shape)
+        proto = preds[1][-1] if len(preds[1]) == 3 else preds[1]
+        # print('proto:', proto.shape)
+        for i, pred in enumerate(p):
             orig_img = orig_img[i] if isinstance(orig_img, list) else orig_img
-            shape = orig_img.shape
-            pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
             path, _, _, _, _ = self.batch
             img_path = path[i] if isinstance(path, list) else path
-            results.append(Results(orig_img=orig_img, path=img_path, names=self.model.names, boxes=pred))
-        # print(results)
+            if not len(pred):  # save empty boxes
+                print('no detections')
+                results.append(Results(orig_img=orig_img, path=img_path, names=self.model.names, boxes=pred[:, :6]))
+                print("None")
+                continue
+
+            if self.args.retina_masks:
+                if not isinstance(orig_img, torch.Tensor):
+                    pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
+            else:
+                masks = ops.process_mask(proto[i], pred[:, 6:], pred[:, :4], img.shape[2:], upsample=True)  # HWC
+                if not isinstance(orig_img, torch.Tensor):
+                    pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], orig_img.shape)
+            results.append(
+                Results(orig_img=orig_img, path=img_path, names=self.model.names, boxes=pred[:, :6], masks=masks))
         return results
 
     def write_results(self, idx, results, batch):
@@ -250,26 +259,53 @@ class YoloPredictor(BasePredictor, QObject):
             im = im[None]  # expand for batch dim
         self.seen += 1
         imc = im0.copy() if self.args.save_crop else im0
-        if self.source_type.webcam or self.source_type.from_img:  # batch_size >= 1         # attention
+        if self.source_type.webcam or self.source_type.from_img:  # batch_size >= 1
             log_string += f'{idx}: '
             frame = self.dataset.count
         else:
             frame = getattr(self.dataset, 'frame', 0)
+
         self.data_path = p
         self.txt_path = str(self.save_dir / 'labels' / p.stem) + ('' if self.dataset.mode == 'image' else f'_{frame}')
-        # log_string += '%gx%g ' % im.shape[2:]         # !!! don't add img size~
+        log_string += '%gx%g ' % im.shape[2:]  # print string
         self.annotator = self.get_annotator(im0)
 
-        det = results[idx].boxes  # TODO: make boxes inherit from tensors
+        result = results[idx]
+        if len(result) == 0:
+            return f'{log_string}(no detections), '
+        det, mask = result.boxes, result.masks  # getting tensors TODO: mask mask,box inherit for tensor
 
-        if len(det) == 0:
-            return f'{log_string}(no detections), ' # if no, send this~~
-
+        # Print results
         for c in det.cls.unique():
             n = (det.cls == c).sum()  # detections per class
-            log_string += f"{n}~{self.model.names[int(c)]},"   #   {'s' * (n > 1)}, "   # don't add 's'
-        # now log_string is the classes 👆
+            log_string += f"{n} {self.model.names[int(c)]}{'s' * (n > 1)}, "
 
+        # Mask plotting
+        if self.args.save or self.args.show:
+            im_gpu = torch.as_tensor(im0, dtype=torch.float16, device=mask.masks.device).permute(
+                2, 0, 1).flip(0).contiguous() / 255 if self.args.retina_masks else im[idx]
+            self.annotator.masks(masks=mask.masks, colors=[colors(x, True) for x in det.cls], im_gpu=im_gpu)
+
+        # Write results
+        for j, d in enumerate(reversed(det)):
+            cls, conf, id = d.cls.squeeze(), d.conf.squeeze(), None if d.id is None else int(d.id.item())
+            if self.args.save_txt:  # Write to file
+                seg = mask.segments[len(det) - j - 1].copy().reshape(-1)  # reversed mask.segments, (n,2) to (n*2)
+                line = (cls, *seg) + (conf, ) * self.args.save_conf + (() if id is None else (id, ))
+                with open(f'{self.txt_path}.txt', 'a') as f:
+                    f.write(('%g ' * len(line)).rstrip() % line + '\n')
+            if self.args.save or self.args.save_crop or self.args.show:  # Add bbox to image
+                c = int(cls)  # integer class
+                name = ('' if id is None else f'id:{id} ') + self.model.names[c]
+                label = None if self.args.hide_labels else (name if self.args.hide_conf else f'{name} {conf:.2f}')
+                self.annotator.box_label(d.xyxy.squeeze(), label, color=colors(c, True)) if self.args.boxes else None
+            if self.args.save_crop:
+                save_one_box(d.xyxy,
+                             imc,
+                             file=self.save_dir / 'crops' / self.model.model.names[c] / f'{self.data_path.stem}.jpg',
+                             BGR=True)
+
+        return log_string
 
         # write
         for d in reversed(det):
